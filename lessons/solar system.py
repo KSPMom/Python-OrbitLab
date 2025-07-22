d6595ceeff88dd0b83343407c64b0656ea0cd142
KSPMom <40582755+KSPMom@users.noreply.github.com>
2025-07-22 17:05:42 +0000

    made all objects exert gravity

--- lessons/solar system.py
+++ lessons/solar system.py
@@ -3,14 +3,18 @@ import math
 
 # Initialize Pygame
 pygame.init()
+print("PROGRAM INITALIZED")
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 600, 600
-d_T = 86400 / 60 # timestep (delta time)
+d_T = 10 * 86400 / 60 # timestep (delta time)
+d_T_int_step = 1000
 G = 6.6743e-11
 EARTH_M = 5.972e+24
 SUN_M = 1.9891e+30
+MOON_M = 7.34767309e+22
 METERS_PER_PIXEL = 1.496e+9
+frames = 0
 
 # Set up the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -33,51 +37,51 @@ class Planet:
         self.color = color
         self.radius = radius
         self.mass = mass
-        self.force = pygame.Vector2(0, 0)
+        self.force_i = pygame.Vector2(0, 0)
         self.pos = pygame.Vector2(x_pos, y_pos)
         self.vel = pygame.Vector2(x_vel, y_vel)
         self.acl = pygame.Vector2(0, 0)
         
-    def update(self):
-        keys = pygame.key.get_pressed()
+    def calc_force(self, other: "Planet"):
+        self.r2 = (self.pos.distance_to(other.pos) * METERS_PER_PIXEL)**2
+        self.theta = 180-(self.pos-other.pos).angle_to(other.pos-other.pos)
+        self.f = ((G * self.mass * other.mass) / self.r2)
+        self.force_i = pygame.math.Vector2.from_polar((self.f, self.theta))
+        return self.force_i
+    
+    def update(self, d_T_int_step, others: list["Planet"]):
 
-        if keys[pygame.K_d]:
-            self.vel += pygame.math.Vector2(0.1,0)
-        
-        if keys[pygame.K_a]:
-            self.vel += pygame.math.Vector2(-0.1,0)
-        
-        if keys[pygame.K_w]:
-            self.vel += pygame.math.Vector2(0,-0.1)
-        
-        if keys[pygame.K_s]:
-            self.vel += pygame.math.Vector2(0,0.1)
+        self.force = None
+        for other in others:
+            try:
+                self.force_i = self.calc_force(other)
+                if self.force is None:
+                    self.force = self.force_i
+                else:
+                    self.force += self.force_i
+            except ZeroDivisionError:
+                continue
 
-        if keys[pygame.K_SPACE]:
-            self.vel = pygame.math.Vector2(0,0)
-        
-
-        self.r2 = (self.pos.distance_to(sun.pos) * METERS_PER_PIXEL)**2
-        self.theta = 180-(self.pos-sun.pos).angle_to(sun.pos-sun.pos)
-        self.f = ((G * self.mass * sun.mass) / self.r2)
-        self.force = pygame.math.Vector2.from_polar((self.f, self.theta))
         self.acl = self.force / self.mass
-        self.vel += self.acl * d_T
-        self.pos += self.vel / METERS_PER_PIXEL * d_T
-        pygame.draw.circle(screen, self.color, self.pos, self.radius)
-        print("vel:",self.vel)
-        print("pos:",self.pos)
-        print("acl:",self.acl)
+        self.vel += self.acl * d_T / d_T_int_step
+        self.pos += self.vel / METERS_PER_PIXEL * d_T / d_T_int_step
         
-
+        #print("vel:",self.vel)
+        #print("pos:",self.pos)
+        #print("acl:",self.acl)
+        
+    def draw(self):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
 # Main loop
 running = True
 
 # things
 earth = Planet(300, 400, 29780, 0, (0,0,255), 2, EARTH_M)
-sun = Star(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, (255, 255, 0), 5, SUN_M)
-
+earth2 = Planet(300, 404, 29780, 0, (0,0,255), 2, SUN_M)
+sun = Planet(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 0, 0, (255, 255, 0), 20, SUN_M)
+luna = Planet(300, 400 + 0.25695552898, 29780 + 1018, 0, (200,200,200), 1, MOON_M)
+bodies = [earth, earth2, sun, luna]
 
 while running:
     for event in pygame.event.get():
@@ -88,8 +92,21 @@ while running:
     
 
     screen.fill((0,0,0))
-    sun.update()
-    earth.update()
+    for i in range(d_T_int_step):
+        earth.update(d_T_int_step, bodies)
+        earth2.update(d_T_int_step, bodies)
+        luna.update(d_T_int_step, bodies)
+        sun.update(d_T_int_step, bodies)
+
+    
+    earth.draw()
+    earth2.draw()
+    luna.draw()
+    sun.draw()
+    frames += 1
+    print("frames elapsed:",frames)
+
+
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
--- lessons/solarsystem2.py
+++ lessons/solarsystem2.py
@@ -6,12 +6,12 @@ pygame.init()
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 600, 600
-d_T = 1 # timestep (delta time)
-#G = 6.6743e-11
-G = 0.5
+d_T = 86400 / 60 # timestep (delta time)
+d_T_inte = 1000
+G = 6.6743e-11
 EARTH_M = 5.972e+24
 SUN_M = 1.9891e+30
-METERS_PER_PIXEL = 1
+METERS_PER_PIXEL = 1.496e+9
 
 # Set up the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -39,47 +39,27 @@ class Planet:
         self.vel = pygame.Vector2(x_vel, y_vel)
         self.acl = pygame.Vector2(0, 0)
         
-    def update(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_d]:
-            self.vel += pygame.math.Vector2(0.1,0)
-        
-        if keys[pygame.K_a]:
-            self.vel += pygame.math.Vector2(-0.1,0)
-        
-        if keys[pygame.K_w]:
-            self.vel += pygame.math.Vector2(0,-0.1)
-        
-        if keys[pygame.K_s]:
-            self.vel += pygame.math.Vector2(0,0.1)
-
-        if keys[pygame.K_SPACE]:
-            self.vel = pygame.math.Vector2(0,0)
-        
-
-        self.r2 = self.pos.distance_squared_to(sun.pos)
+    def update(self, d_T_inte: float):
+        self.r2 = (self.pos.distance_to(sun.pos) * METERS_PER_PIXEL)**2
         self.theta = 180-(self.pos-sun.pos).angle_to(sun.pos-sun.pos)
         self.f = ((G * self.mass * sun.mass) / self.r2)
         self.force = pygame.math.Vector2.from_polar((self.f, self.theta))
-        # self.force =  (sun.pos - self.pos) * self.f
-        # self.force = pygame.math.Vector2(self.force.x, self.force.y)
         self.acl = self.force / self.mass
-        self.vel += self.acl * d_T
-        self.pos += self.vel * d_T
-        pygame.draw.circle(screen, self.color, (self.pos), self.radius)
+        self.vel += self.acl * d_T / d_T_inte
+        self.pos += self.vel / METERS_PER_PIXEL * d_T / d_T_inte
+        print("vel:",self.vel)
+        print("pos:",self.pos)
+        print("acl:",self.acl)
         
-
+    def draw(self):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
 # Main loop
 running = True
 
 # things
-earth = Planet(300, 500, 1.5, 0, (0,0,255), 10, 100)
-calidor = Planet(300, 400, 2.2, 0, (255,0,0), 8, 78)
-sun = Star(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, (255, 255, 0), 50, 1000)
-interloper = Planet(300, 350, 4.2, 0, (200,200,255), 2, 15)
-
+earth = Planet(300, 400, 29780, 0, (0,0,255), 2, EARTH_M)
+sun = Star(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, (255, 255, 0), 5, SUN_M)
 
 
 while running:
@@ -92,9 +72,11 @@ while running:
 
     screen.fill((0,0,0))
     sun.update()
-    earth.update()
-    calidor.update()
-    interloper.update()
+    for i in d_T_inte:
+        earth.update(d_T_inte)
+
+    
+    earth.draw()
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
