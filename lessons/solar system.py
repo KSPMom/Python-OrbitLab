4d6fe7b3d9b2f9d5c11cf2bc370c1a0614eab083
KSPMom <40582755+KSPMom@users.noreply.github.com>
2025-07-24 17:01:45 +0000

    added camera controls

--- lessons/solar system.py
+++ lessons/solar system.py
@@ -7,7 +7,7 @@ print("PROGRAM INITALIZED")
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 600, 600
-d_T = 10 * 86400 / 60 # timestep (delta time)
+d_T = 100 * 86400 / 60 # timestep (delta time)
 d_T_int_step = 1000
 G = 6.6743e-11
 EARTH_M = 5.972e+24
@@ -15,22 +15,12 @@ SUN_M = 1.9891e+30
 MOON_M = 7.34767309e+22
 METERS_PER_PIXEL = 1.496e+9
 frames = 0
+offset = pygame.math.Vector2(0,0)
+track_CoM = False
 
 # Set up the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Solar System")
-
-class Star:
-    def __init__(self, x_pos: float, y_pos: float, color: tuple, radius: int, mass: int):
-        self.color = color
-        self.radius = radius
-        self.mass = mass
-        self.pos = pygame.Vector2(x_pos, y_pos)
-        
-
-
-    def update(self):
-        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
 class Planet:
     def __init__(self, x_pos: float, y_pos: float, x_vel: float, y_vel: float, color: tuple, radius: int, mass: int):
@@ -71,15 +61,15 @@ class Planet:
         #print("acl:",self.acl)
         
     def draw(self):
-        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+        pygame.draw.circle(screen, self.color, self.pos + offset, self.radius)
+
 
 # Main loop
 running = True
 
 # things
 earth = Planet(300, 400, 29780, 0, (0,0,255), 2, EARTH_M)
-#earth2 = Planet(300, 404, 29780, 0, (0,0,255), 2, SUN_M)
-sun = Planet(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 0, 0, (255, 255, 0), 20, SUN_M)
+sun = Planet(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 1000, 0, (255, 255, 0), 20, SUN_M)
 luna = Planet(300, 400 + 0.25695552898, 29780 + 1018, 0, (200,200,200), 1, MOON_M)
 bodies = [earth, sun, luna]
 
@@ -94,17 +84,34 @@ while running:
     screen.fill((0,0,0))
     for i in range(d_T_int_step):
         earth.update(d_T_int_step, bodies)
-        #earth2.update(d_T_int_step, bodies)
         luna.update(d_T_int_step, bodies)
         sun.update(d_T_int_step, bodies)
-
+    
+    keys = pygame.key.get_pressed()
+    CoM = ((sun.pos * sun.mass + earth.pos * earth.mass + luna.pos * luna.mass) / (sun.mass + earth.mass + luna.mass))
+    if keys[pygame.K_a]:
+        offset += pygame.math.Vector2(10,0)
+    if keys[pygame.K_d]:
+        offset += pygame.math.Vector2(-10,0)
+    if keys[pygame.K_w]:
+        offset += pygame.math.Vector2(0,10)
+    if keys[pygame.K_s]:
+        offset += pygame.math.Vector2(0,-10)
+    if keys[pygame.K_SPACE]:
+        if track_CoM == False:
+            track_CoM = True
+        else:
+            track_CoM = False
+    if track_CoM == True:
+        offset = -(CoM - pygame.math.Vector2(300,300))
+    print(track_CoM)
+    
     
     earth.draw()
-    #earth2.draw()
     luna.draw()
     sun.draw()
     frames += 1
-    print("frames elapsed:",frames)
+    #print("frames elapsed:",frames)
 
 
     pygame.display.flip()
